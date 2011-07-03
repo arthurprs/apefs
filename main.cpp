a8dc5772a2f55d2efb605e42a5adcb19fd9165d3
unknown <Arthur@.(none)>
2011-07-03 11:59:17 -0300

    - Updated test program

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <string>
 #include <dirent.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include "apefs/apefilesystem.h"
 
 using namespace std;
+
+void makedir(const string& dir)
+{
+#ifdef WIN32
+    mkdir(dir.c_str());
+    //system(("mkdir \"" + dir + "\"").c_str());
+#else
+    mkdir(dir.c_str(), 777);
+#endif
+}
 
 void printfs(ApeFileSystem& fs, const string &path = "/", int level = 0)
 {
@@ -30,26 +41,26 @@ void printfs(ApeFileSystem& fs, const string &path = "/", int level = 0)
 
 bool backupfile(const string& filepath, const string& backuppath, ApeFileSystem& fs)
 {
-	fstream file;
-	file.open(filepath.c_str(), ios::in | ios::out | ios::binary);
-	if (!file.good())
-		return false;
+    fstream file;
+    file.open(filepath.c_str(), ios::in | ios::out | ios::binary);
+    if (!file.good())
+        return false;
 
-	ApeFile bfile(fs);
-	if (!bfile.open(backuppath.c_str(), APEFILE_CREATE))
-		return false;
+    ApeFile bfile(fs);
+    if (!bfile.open(backuppath.c_str(), APEFILE_CREATE))
+        return false;
 
-	char buffer[1024];
-	int count = 0;
-	while (!file.eof())
-	{
-		file.read(buffer, sizeof(buffer));
-		count += file.gcount();
-		if (bfile.write(buffer, file.gcount()) != file.gcount())
-			return false;
-	}
+    char buffer[1024];
+    int count = 0;
+    while (!file.eof())
+    {
+        file.read(buffer, sizeof(buffer));
+        count += file.gcount();
+        if (bfile.write(buffer, file.gcount()) != file.gcount())
+            return false;
+    }
 
-	return true;
+    return true;
 }
 
 bool backup(const string& srcpath, ApeFileSystem& fs, const string& relpath = "/")
@@ -57,58 +68,58 @@ bool backup(const string& srcpath, ApeFileSystem& fs, const string& relpath = "/
     dirent *entry;
     DIR *dp;
 
-	dp = opendir(srcpath.c_str());
+    dp = opendir(srcpath.c_str());
     if (dp == NULL)
-		return false;
+        return false;
 
     while((entry = readdir(dp)))
-	{
-	    struct stat s;
-	    stat(fs.joinpath(srcpath, entry->d_name).c_str(), &s);
+    {
+        struct stat s;
+        stat(fs.joinpath(srcpath, entry->d_name).c_str(), &s);
 
-		if (S_ISDIR(s.st_mode))
-		{
-			if (entry->d_name[0] == '.')
-				continue;
-			if (!fs.directorycreate(relpath + entry->d_name))
-				return false;
-			if (!backup(fs.joinpath(srcpath, entry->d_name), fs, fs.joinpath(relpath, entry->d_name)))
-				return false;
-		}
-		else
-		{
-			if (!backupfile(fs.joinpath(srcpath, entry->d_name), fs.joinpath(relpath, entry->d_name), fs))
-				return false;
-		}
-	}
+        if (S_ISDIR(s.st_mode))
+        {
+            if (entry->d_name[0] == '.')
+                continue;
+            if (!fs.directorycreate(relpath + entry->d_name))
+                return false;
+            if (!backup(fs.joinpath(srcpath, entry->d_name), fs, fs.joinpath(relpath, entry->d_name)))
+                return false;
+        }
+        else
+        {
+            if (!backupfile(fs.joinpath(srcpath, entry->d_name), fs.joinpath(relpath, entry->d_name), fs))
+                return false;
+        }
+    }
 
     closedir(dp);
 
-	return true;
+    return true;
 }
 
 bool restorefile(const string& backuppath, const string& filepath, ApeFileSystem& fs)
 {
-	fstream file;
-	file.open(filepath.c_str(), ios::in | ios::out | ios::binary | ios::trunc);
-	if (!file.good())
-		return false;
+    fstream file;
+    file.open(filepath.c_str(), ios::in | ios::out | ios::binary | ios::trunc);
+    if (!file.good())
+        return false;
 
-	ApeFile bfile(fs);
-	if (!bfile.open(backuppath, APEFILE_OPEN))
-		return false;
+    ApeFile bfile(fs);
+    if (!bfile.open(backuppath, APEFILE_OPEN))
+        return false;
 
-	char buffer[1024];
+    char buffer[1024];
 
-	while (bfile.tell() < bfile.size())
-	{
-		int count = bfile.read(buffer, sizeof(buffer));
-		file.write(buffer, count);
-		if (!file.good())
-			return false;
-	}
+    while (bfile.tell() < bfile.size())
+    {
+        int count = bfile.read(buffer, sizeof(buffer));
+        file.write(buffer, count);
+        if (!file.good())
+            return false;
+    }
 
-	return true;
+    return true;
 }
 
 bool restore(const string& dstpath, ApeFileSystem& fs, const string& srcpath = "/")
@@ -119,67 +130,68 @@ bool restore(const string& dstpath, ApeFileSystem& fs, const string& srcpath = "
     {
         if (entries[i].isdirectory())
         {
-			mkdir(fs.joinpath(dstpath, entries[i].name).c_str());
+            makedir(fs.joinpath(dstpath, entries[i].name));
             if (!restore(fs.joinpath(dstpath, entries[i].name), fs, fs.joinpath(srcpath, entries[i].name)))
-				return false;
+                return false;
         }
         else
         {
-			if (!restorefile(fs.joinpath(srcpath, entries[i].name), fs.joinpath(dstpath, entries[i].name), fs))
-				return false;
+            if (!restorefile(fs.joinpath(srcpath, entries[i].name), fs.joinpath(dstpath, entries[i].name), fs))
+                return false;
         }
     }
 
-	return true;
-}
-
-
-void usage()
-{
-	cout << "Usage: backup.exe \"backupfolder\" \"restorefolder\"" << endl;
+    return true;
 }
 
 int main(int argc, char **argv)
 {
     ApeFileSystem fs;
     string backupfs = "backup.apefs";
-	string restorefolder;
+    string restorefolder;
     string backupfolder;
 
-	if (argc < 3)
-	{
-		usage();
-		getchar();
-		exit(1);
-	}
+    if (argc > 3)
+    {
+        backupfolder = argv[1];
+        restorefolder = argv[2];
+    }
+    else
+    {
+        char buffer[255];
+        cout << "Backup folder:\n> ";
+        cin.getline(buffer, 255);
+        backupfolder = buffer;
 
-    backupfolder = argv[1];
-	restorefolder = argv[2];
+        cout << "Restore folder:\n> ";
+        cin.getline(buffer, 255);
+        restorefolder = buffer;
+    }
 
-	cout << "Backup in progress..." << endl;
+    cout << "Backup in progress..." << endl;
 
     if (!fs.create(backupfs, 1024 * 1024 * 100)) // 100 mb
-	{
-		cout << "Couldn't create filesystem\n";
-		getchar();
-		exit(1);
-	}
+    {
+        cout << "Couldn't create filesystem\n";
+        getchar();
+        exit(1);
+    }
 
-	if (backup(backupfolder, fs))
-		cout << "Backup ok!" << endl << endl;
-	else
-		cout << "Backup error!" << endl << endl;
-
-    fs.close();
-
-
-	cout << "Restoring..." << endl;
-    fs.open(backupfs);
-	mkdir(restorefolder.c_str());
-	if (restore(restorefolder, fs))
-		cout << "Restore ok!" << endl << endl;
-	else
-		cout << "Restore error!" << endl << endl;
+    if (backup(backupfolder, fs))
+    {
+        cout << "Backup ok!" << endl << endl;
+        cout << "Restoring..." << endl;
+        fs.open(backupfs);
+        makedir(restorefolder);
+        if (restore(restorefolder, fs))
+            cout << "Restore ok!" << endl << endl;
+        else
+            cout << "Restore error!" << endl << endl;
+    }
+    else
+    {
+        cout << "Backup error!" << endl << endl;
+    }
 
     getchar();
     return 0;
